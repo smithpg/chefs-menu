d60c7c00a45471219e5582d719df3fed0e9260c8
patrickgsmith <patrickgromersmith@gmail.com>
2019-09-15 11:57:40 -0700

    horizontally centered cuisine tags

--- client/src/components/CuisineList.js
+++ client/src/components/CuisineList.js
@@ -1,26 +1,20 @@
-import CuisineTag from "./CuisineTag"
+import CuisineTag from "./CuisineTag";
 import React from "react";
-import { makeStyles } from '@material-ui/core/styles';
+import { makeStyles } from "@material-ui/core/styles";
 
 const useStyles = makeStyles({
-
-    wrap:{
-
-        display:"flex",
-        flexWrap:"wrap",
-    }
+  wrap: {
+    justifyContent: "center",
+    display: "flex",
+    flexWrap: "wrap"
+  }
 });
 
 export default function Cuisinelist({ cuisineList }) {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    const cuisineComponents = cuisineList.map((name,i) =>{
-        return <CuisineTag name={cuisineList[i]}/>;
-    });
-    return (
-        <div className={classes.wrap}>
-            {cuisineComponents}
-
-        </div>
-    );
+  const cuisineComponents = cuisineList.map((name, i) => {
+    return <CuisineTag name={cuisineList[i]} />;
+  });
+  return <div className={classes.wrap}>{cuisineComponents}</div>;
 }
